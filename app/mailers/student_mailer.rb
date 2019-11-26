c30d1dd9e58c3438fdc684a1bd720e8e23157e65
Abinash Kumar Shaw <abinashshaw16@gmail.com>
2019-11-26 16:24:14 +0530

    Added instruction about using this functionality

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -7,7 +7,10 @@ class StudentMailer < ApplicationMailer
   #
   def students_record(students)
     @students = students
-
-    mail to: "abinashshaw16@gmail.com", subject: "Students Record"
+    # give the receiver's mail address below mail will be sent from the mail id abinashshaw16@gmail.com and 
+    # i have set up a specific password for the rails app learn_rails to overcome the issue of less secure 
+    # app. go to https://guides.rubyonrails.org/action_mailer_basics.html#action-mailer-configuration-for-gmail
+    # to know more
+    mail to: "abinashshaw16@gmail.com", subject: "Students Record ordered by name"
   end
 end
