1d4a8164c81f65a9a5ba7a0f0d5dc2d1e4e47862
Abinash Kumar Shaw <abinashshaw16@gmail.com>
2019-11-28 16:28:18 +0530

    Created a Customer model with CRUD functionality made it very DRY

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,45 @@
+class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @customers = Customer.all
+  end
+  def new
+    @customer = Customer.new
+  end
+  def show
+  end
+  def create
+    #render plain: params[:customer].inspect
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      flash[:notice] = "Customer was successfully created"
+      redirect_to customer_path(@customer)
+    else
+      render 'new'
+    end
+  end
+  def edit 
+  end
+  def update
+    if Customer.update(customer_params)
+      flash[:notice] = "Customer was updated successfully"
+      redirect_to customer_path(@customer)
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @customer.destroy
+    flash[:notice] = "Customer is successfully deleted"
+    redirect_to customers_path
+  end
+
+  private
+    def set_customer
+      @customer = Customer.find(params[:id])
+    end
+    def customer_params
+      params.require(:customer).permit(:name,:email)
+    end
+end
---
+++ app/models/customer.rb
@@ -0,0 +1,6 @@
+class Customer < ApplicationRecord
+    validates :name, presence: true, uniqueness: {case_sensitive: false}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: {case_sensitive: false},
+                      format: {with: VALID_EMAIL_REGEX}
+end
